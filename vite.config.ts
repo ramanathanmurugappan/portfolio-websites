b4e74bf2a410380b813b08f7b699ca9f6bf0f940
ramanathan <ramanathanrobotist@gmail.com>
2026-01-26 16:36:58 +0530

    Setup custom domain ram96.com

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 const plugins = [react(), tailwindcss()];
 
 export default defineConfig({
-  base: "/protfolio/",
+  base: "/",
   plugins,
   resolve: {
     alias: {
